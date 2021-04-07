1288f9a612e00d3f962c157aa2f6d7a1da58cb7a
vishwajit2 <vishwajitkadam13@mail.com>
2021-04-07 11:41:27 +0530

    script to compile and run project

---
+++ run.sh
@@ -0,0 +1,21 @@
+#  to compile
+
+cd binaryTree  
+javac  BSTInterface.java BSTNode.java
+
+cd ../wiredBST
+javac -cp ../ WiredBST.java
+
+cd ../ioHandler
+javac -cp ../ IOHandler.java
+
+cd ../student
+javac Student.java
+
+cd ../treeGUI
+javac -cp ../ TreePrinter.java GUIApplication.java
+
+#  To execute compiled code, run following commands
+
+# cd ../main
+# javac -cp ../ Main.java
